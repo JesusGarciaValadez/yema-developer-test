abaf7b22bc8716878a6c80751d839491ee5c1ad0
Jesús Antonio García Valadez <jesus.garciav@me.com>
2020-07-05 21:45:29 -0500

    feat: add .idea folder to .gitignore

---
+++ pages/404.js
@@ -0,0 +1,10 @@
+const Custom404 = () => (
+    <div>
+        <h1 style="display:inline-block;border-right:1px solid rgba(0, 0, 0,.3);margin:0;margin-right:20px;padding:10px 23px 10px 0;font-size:24px;font-weight:500;vertical-align:top">404</h1>
+        <div style="display:inline-block;text-align:left;line-height:49px;height:49px;vertical-align:middle">
+            <h2 style="font-size:14px;font-weight:normal;line-height:inherit;margin:0;padding:0">This page could not be found.</h2>
+        </div>
+    </div>
+)
+
+export default Custom404
