9a801d88d96975f1eac815941bc3f45265d3b852
Jesús Antonio García Valadez <jesus.garciav@me.com>
2020-07-05 21:46:44 -0500

    feat: add 404 custom page

--- pages/404.js
+++ pages/404.js
@@ -1,10 +1,20 @@
-const Custom404 = () => (
-    <div>
-        <h1 style="display:inline-block;border-right:1px solid rgba(0, 0, 0,.3);margin:0;margin-right:20px;padding:10px 23px 10px 0;font-size:24px;font-weight:500;vertical-align:top">404</h1>
-        <div style="display:inline-block;text-align:left;line-height:49px;height:49px;vertical-align:middle">
-            <h2 style="font-size:14px;font-weight:normal;line-height:inherit;margin:0;padding:0">This page could not be found.</h2>
-        </div>
-    </div>
-)
+import Layout from "../components/Layout";
+import Head from "next/head";
+import React from "react";
 
-export default Custom404
+export default function Custom404() {
+    return (
+        <Layout>
+            <Head>
+                <title>404 This page could not found | Yema Developer Test App</title>
+            </Head>
+
+            <div className="flex flex-row max-w-7xl mx-auto py-6 sm:px-6 lg:px-8 p-10 text-md font-light tracking-normal leading-normal">
+                <h1 style={ { display: 'inline-block', borderRight: '1px solid rgba(0, 0, 0,.3)', margin: '0', marginRight: '20px', padding: '10px 23px 10px 0', fontSize: '24px', fontWeight: '500', verticalAlign: 'top' } }>404</h1>
+                <div style={ { display: 'inline-block', textAlign: 'left', lineHeight: '49px', height: '49px', verticalAlign: 'middle' } }>
+                    <h2 style={ { fontSize: '14px', fontWeight: 'normal', lineHeight: 'inherit', margin: '0', padding:'0' } }>This page could not be found.</h2>
+                </div>
+            </div>
+        </Layout>
+    )
+}
